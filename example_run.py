a55b5562c9baf96446c3b35d057dd21b3e734b6d
Anonymous <anonymous@example.tld>
2014-04-04 16:41:22 -0400

    Fix example run.

--- example_run.py
+++ example_run.py
@@ -1,2 +1,2 @@
 import exec6816
-exec6816.exec6816('cqsub', 'java ParallelPacket 2000 [n] [w] false 1 8 [l] [s]', {'w': [1000, 2000], 'n': [2, 4]})
+exec6816.exec6816('cqsub', 'java ParallelPacket 2000 [n] [w] false 1 8 [l] [s]', {'w': [1000, 2000], 'n': [2, 4], 'l': [0], 's': [0, 1]})
